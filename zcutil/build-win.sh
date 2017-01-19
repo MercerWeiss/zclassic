081a4921668716839738a8e24d1728e517424c4c
David Mercer <radix42@gmail.com>
2017-01-19 14:12:28 -0700

    update build script for win

--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -4,9 +4,8 @@ cd depends
 make
 cd ..
 ./augogen.sh
-CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/  --with-gnu-ld --enable-static --disable-shared CXX=x86_64-w64-mingw32-g++-posix CC=x86_64-w64-mingw32-gcc-posix
+CXX=x86_64-w64-mingw32-g++-posix CC=x86_64-w64-mingw32-gcc-posix CXXFLAGS+="  -fopenmp" CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site ./configure --prefix=/  --with-gnu-ld --enable-static --disable-shared CXX=x86_64-w64-mingw32-g++-posix CC=x86_64-w64-mingw32-gcc-posix --host=x86_64-w64-mingw32
 cd src
 sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' Makefile
 cd ..
-make
-
+CXX=x86_64-w64-mingw32-g++-posix CC=x86_64-w64-mingw32-gcc-posix make
